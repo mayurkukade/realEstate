e161aba1aaf40b2ec7e2e8a47adfc587f567432d
Sumedhk18 <sumedhkamalapurkar@opticalarc.com>
2024-03-14 22:34:15 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import { Footer } from './Component/Footer';
+import About from './Component/About';
 
 function App() {
 
 
   return (
     <>
-    <Footer/>
+    <About/>
     </>
   )
 }
